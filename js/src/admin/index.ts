1811defc08adf719bf50ea280435a0a699c69640
Rafał Całka <calka.rafal@gmail.com>
2021-12-22 07:07:55 +0100

    Update dependencies and convert to TypeScript

---
+++ js/src/admin/index.ts
@@ -0,0 +1,9 @@
+import app from 'flarum/admin/app';
+
+app.initializers.add('flarum-akismet', () => {
+  app.extensionData.for('flarum-akismet').registerSetting({
+    setting: 'flarum-akismet.api_key',
+    type: 'text',
+    label: app.translator.trans('flarum-akismet.admin.akismet_settings.api_key_label'),
+  });
+});
